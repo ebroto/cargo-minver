9dba1db46643508ba0fe80d187586afd2ff1cee4
Eduardo Broto <ebroto@tutanota.com>
2020-03-02 01:04:36 +0100

    Initial integration test infrastructure
    Add a project builder to create cargo projects under /tmp
    Disable unit tests explicitly in Cargo.toml

---
+++ tests/util/project.rs
@@ -0,0 +1,70 @@
+use std::path::PathBuf;
+use std::{env, fs};
+
+use anyhow::Result;
+use tempfile::TempDir;
+
+pub struct Builder {
+    name: String,
+    source_files: Vec<PathBuf>,
+}
+
+impl Builder {
+    pub fn new(name: &str) -> Self {
+        Self { name: name.into(), source_files: Vec::new() }
+    }
+
+    pub fn with_source_file<P: Into<PathBuf>>(&mut self, path: P) -> Result<&mut Self> {
+        let mut target = env::current_dir()?;
+        target.push("tests");
+        target.push(path.into());
+
+        self.source_files.push(target);
+        Ok(self)
+    }
+
+    pub fn create(&mut self) -> Result<Project> {
+        if self.source_files.is_empty() {
+            panic!("project must have at least one source file")
+        }
+
+        let project_dir = TempDir::new()?;
+        let src_dir = project_dir.path().join("src");
+        fs::create_dir(&src_dir)?;
+
+        for (num, source_file) in self.source_files.iter().enumerate() {
+            let file_name: PathBuf = if num == 0 { "main.rs".into() } else { source_file.file_name().unwrap().into() };
+
+            let mut dest = src_dir.clone();
+            dest.push(file_name);
+            fs::copy(source_file, dest)?;
+        }
+
+        let manifest_path = project_dir.path().join("Cargo.toml");
+        let manifest = format!(
+            r#"[package]
+name = "{}"
+version = "0.0.0"
+"#,
+            self.name
+        );
+        fs::write(manifest_path, manifest)?;
+
+        Ok(Project { _name: self.name.clone(), dir: project_dir })
+    }
+}
+
+pub struct Project {
+    _name: String,
+    dir: TempDir,
+}
+
+impl Project {
+    pub fn _name(&self) -> &str {
+        &self._name
+    }
+
+    pub fn manifest_path(&self) -> PathBuf {
+        self.dir.path().join("Cargo.toml")
+    }
+}
